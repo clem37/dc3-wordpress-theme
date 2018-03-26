f45df178a3039074c99359d65ffac0dde6262c66
Clem <clem.chassaing@gmail.com>
2018-03-26 16:27:46 +0200

    Fichiers modifications

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
+<?php get_header(); ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
-Theme Name: dc3-wordpress-theme
-Author: Clémence et Céline
-Description: Template WordPress pour cours de dev
-Version: 0.0.1
-Tags: wordpress
-*/
+<?php get_footer(); ?>
